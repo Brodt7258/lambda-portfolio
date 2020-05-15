0a1e3a1d0b951251c4b84aa2453cb7a11778cd00
Tim Dill <brodt7258@gmail.com>
2020-05-14 23:46:01 -0400

    Build out basic styled for project card

--- src/App.js
+++ src/App.js
@@ -1,30 +1,84 @@
 import React from 'react';
 import './App.css';
+import { Paper } from '@material-ui/core';
+import styled, { createGlobalStyle } from 'styled-components';
+import img from './img/dbd5250c0e451f3cefc0bf05bd85a928.png';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: darkgray;
+  }
+`;
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  padding: 0 2rem;
+  align-items: center;
+
+  & > * {
+    margin: 1rem 0;
+  }
+`;
+
+const Header = styled(Paper)`
+  padding: 1rem;
+  margin: 0 0 1rem 0;
+`;
+
+const ProjectCard = styled(Paper)`
+  padding: 1rem;
+  width: 100%;
+
+  img {
+    max-width: 100%;
+    object-fit: contain !important;
+  }
+
+  .links {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .project-header {
+    text-align: center;
+  }
+`;
 
 function App() {
   return (
     <>
-      <header>
-        <h1>Portfolio</h1>
-        <img src="" alt="Me?" />
+      <GlobalStyle />
+      <Header>
         <div>
-          Bio
+          <p>Tim Dill</p>
+          <p>Full-stack javascript developer near Hartford, CT</p>
+          <p>Skilled with: HTML, CSS, React, Express</p>
+          <p>tbdill@gmail.com Github Linkedin</p>
         </div>
-        <div>
-          Contact
-        </div>
-      </header>
-      <main>
-        <section>
-          <h2>Project Name</h2>
-          <img src="" alt="there's some sort of img here. Project-related" />
-          <p>A brief description of what I worked on, and what it does</p>
-          <div>
+      </Header>
+      <Main>
+        <ProjectCard>
+          <h2 className="project-header">
+            Foodie Fun
+          </h2>
+          <img src={img} alt="there's some sort of img here. Project-related" />
+          <p>
+            This was the first project I created with Lambda.
+            It&apos;s a landing page for a food review app.
+            It was my first experience with animation, built with Javascript and Greensock
+          </p>
+          <div className="links">
             <p>Deployment</p>
             <p>GitHub</p>
           </div>
-        </section>
-        <section>
+        </ProjectCard>
+        <ProjectCard>
           <h2>Project Name 2</h2>
           <img src="" alt="there's some sort of img here. Project-related" />
           <p>A brief description of what I worked on, and what it does</p>
@@ -32,8 +86,8 @@ function App() {
             <p>Deployment</p>
             <p>GitHub</p>
           </div>
-        </section>
-        <section>
+        </ProjectCard>
+        <ProjectCard>
           <h2>Project Name 3</h2>
           <img src="" alt="there's some sort of img here. Project-related" />
           <p>A brief description of what I worked on, and what it does</p>
@@ -41,8 +95,8 @@ function App() {
             <p>Deployment</p>
             <p>GitHub</p>
           </div>
-        </section>
-        <section>
+        </ProjectCard>
+        <ProjectCard>
           <h2>Project Name 4</h2>
           <img src="" alt="there's some sort of img here. Project-related" />
           <p>A brief description of what I worked on, and what it does</p>
@@ -50,8 +104,8 @@ function App() {
             <p>Deployment</p>
             <p>GitHub</p>
           </div>
-        </section>
-        <section>
+        </ProjectCard>
+        <ProjectCard>
           <h2>Project Name 5</h2>
           <img src="" alt="there's some sort of img here. Project-related" />
           <p>A brief description of what I worked on, and what it does</p>
@@ -59,8 +113,8 @@ function App() {
             <p>Deployment</p>
             <p>GitHub</p>
           </div>
-        </section>
-      </main>
+        </ProjectCard>
+      </Main>
     </>
   );
 }
